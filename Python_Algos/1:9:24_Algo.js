d252c29476bda0e1649b76ed7262242a270ee7b7
amiljour <austinmiljour@gmail.com>
2024-01-09 08:34:24 -0800

    Update

--- Python_Algos/1:9:24_Algo.js
+++ Python_Algos/1:9:24_Algo.js
@@ -35,22 +35,33 @@ const expected4 = "GIT";
 
 function acronymize(str) {
     // Step 1: trim the input string to remove any extra spaces at the start and the end - .trim()
+
     str.trim()
+
     // Step 2: Initialize a variable to hold the acronym, initially empty
+
     let acronym = ''
+
     // Step 3: Split the input into an array of words - .split()
+
     let y = str.split('')
+    console.log(y)
+
     // Step 4: Loop through each word in the array
     //   Step 4.1: Check if the current word is not an empty string
     //   Step 4.2: Take the first character of the word, convert it to uppercase, and add it to the acroynm
+
     acronym += y[0]
     for (var x = 0; x < y.length; x++) {
         if (y[x] === " ") {
             acronym += y[x + 1]
         }
     }
+
     //Step 5: Return the constructed acroynm
+
     return acronym.toUpperCase()
+
 }
 
 const result1 = acronymize(str1);
