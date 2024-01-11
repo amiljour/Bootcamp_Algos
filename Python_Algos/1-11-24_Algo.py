7ba3cdaf5fe4cb5a75baecac3452f838480623b7
amiljour <austinmiljour@gmail.com>
2024-01-11 09:25:16 -0800

    Update

--- Python_Algos/1-11-24_Algo.py
+++ Python_Algos/1-11-24_Algo.py
@@ -5,7 +5,6 @@
 #     - palindrome = string that is same forwards and backwards
 
 # Do not ignore spaces, punctuation and capitalization
-#  */
 
 str1 = "a x a"
 expected1 = True
@@ -27,25 +26,15 @@ expected4 = False
 #    * @returns {boolean} Whether the given str is a palindrome or not.
 #    */
 def isPalindrome(str):
-
-    # Create anempty string to hold the backwards string
     backwards_string = ''
-
-    # Create y to be str length - 1 to be the index
-    y = len(str) - 1
-
-    length = len(str)
-
-    # Create a for loop to iterate throught the lenght of the string
-    while index < len(str):
-        # Adds the last letter to backwardsString
-        backwardsString = backwardsString + str[y]
-        # Minus 1 from y to get the next letter next time it goes through the str
+    y = (len(str) - 1)
+    for index in str:
+        backwards_string += str[y]
         y -= 1
 
-    # Creates a if statement to check if it is the same and if it its = it will return true and if its != it will return false
-    if backwardsString == str:
+    if backwards_string == str:
         return True
+
     else:
         return False
 
